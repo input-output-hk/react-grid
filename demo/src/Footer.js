6e25840b3a5bfa8467e29d9b85ed1c4378b6074c
Lewis Nelson <lewis.nelson@iohk.io>
2019-07-02 15:49:06 +0100

    Updated footer GitHub image reference

--- demo/src/Footer.js
+++ demo/src/Footer.js
@@ -15,7 +15,7 @@ const Footer = () => (
   <footer className='page-footer' style={FooterStyle}>
     <div className='container center-align'>
       <a className='white-text' href='https://github.com/input-output-hk/react-grid' target='noopener noreferrer'>
-        <img src='/images/GitHub-mark.png' alt='GitHub' style={ImageStyle} /> Source code
+        <img src='images/GitHub-mark.png' alt='GitHub' style={ImageStyle} /> Source code
       </a>
     </div>
   </footer>
